5060a09eb513fc3e8e5957312b9d9d3ec9afb421
boycaught <github.robot@gmail.com>
2013-05-29 15:08:30 -0700

    edits to custom script

--- lag_custom.sh
+++ lag_custom.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
-# Run this file from user root, i.e.: ~/ source /etc/profile.d/lag_custom.sh
-# Create link to 'custom.sh' 
-
-## USER BASE
-USER="/Users/greenla"
-
 ## BASE PATH
-MY_HOME="$USER/usr/local"
+MY_HOME="/Users/greenla/usr/local"
 
 ## JAVA PATHS
 JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home"
@@ -18,10 +12,10 @@ ADOBE_AIR_SDK="$MY_HOME/adobeairsdk"
 ANT_HOME="$MY_HOME/ant"
 BREW_HOME="/usr/local"
 CATALINA_HOME="$MY_HOME/catalina"
-CF_HOME="$USER/Applications/ColdFusion10"
+CF_HOME="/Users/greenla/Applications/ColdFusion10"
 CF_ROOT="$CF_HOME/wwwroot/WEB-INF"
-EC2_HOME="$MY_HOME/ec2"
 EC2_CERT="$EC2_HOME/x509_certificate.pem"
+EC2_HOME="$MY_HOME/ec2"
 EC2_PRIVATE_KEY="$EC2_HOME/private_key.pem"
 EC2_URL="http://ec2.amazonaws.com"
 FLEX_SDK="$MY_HOME/flexsdk"
@@ -38,17 +32,17 @@ MONGODB_HOME="$BREW_HOME/Cellar/mongodb/2.4.3-x86_64"
 NGINX_HOME="$BREW_HOME/Cellar/nginx/1.4.1"
 PYTHON_HOME="$BREW_HOME/Cellar/python/2.7.3"
 RUBY_HOME="$BREW_HOME/Cellar/ruby193/1.9.3-p392"
-SUBLIMETEXT="$USER/Applications/Sublime Text 2.app/Contents/SharedSupport"
+SUBLIMETEXT="/Users/greenla/Applications/Sublime Text 2.app/Contents/SharedSupport"
 WEBMIN_HOME="$MY_HOME/webmin"
 YUICOMPRESSOR="$MY_HOME/yuicompressor-2.4.7/compress.jar"
 BOGUS="A whole lot of who?"
 
 CLASSPATH="$JYTHON_HOME/jython.jar:\
-$USER/Applications/ColdFusion10/cfusion/lib/customlib/h2.jar:\
-$USER/Applications/ColdFusion10/cfusion/lib/customlib/hsqldb.jar:\
-$USER/Applications/ColdFusion10/cfusion/lib/customlib/jtds-1.2.5.jar:\
-$USER/Applications/ColdFusion10/cfusion/lib/customlib/mongo-2.7.0.jar:\
-$USER/Applications/ColdFusion10/cfusion/lib/customlib/sqlitejdbc-v037-nested.jar:\
+Users/greenla/Applications/ColdFusion10/cfusion/lib/customlib/h2.jar:\
+Users/greenla/Applications/ColdFusion10/cfusion/lib/customlib/hsqldb.jar:\
+Users/greenla/Applications/ColdFusion10/cfusion/lib/customlib/jtds-1.2.5.jar:\
+Users/greenla/Applications/ColdFusion10/cfusion/lib/customlib/mongo-2.7.0.jar:\
+Users/greenla/Applications/ColdFusion10/cfusion/lib/customlib/sqlitejdbc-v037-nested.jar:\
 $CLASSPATH"
 
 JYTHONPATH="$JYTHON_HOME:$JYTHON_HOME/bin:$JYTHON_HOME/Lib"
@@ -119,7 +113,7 @@ export CC=gcc-4.2
 
 export PATH
 
-## MY CORE ALIASES
+## MY ALIASES
 alias cl='clear'
 alias ll='ls -lag | more'
 alias px='ps Aue'
@@ -129,19 +123,25 @@ alias jy='jython manage.py'
 alias customize='source /etc/profile.d/lag_custom.sh'
 
 ## Shit from Paul Irish (Google webdev guru)
-#
+##
 # Load ~/.extra, ~/.bash_prompt, ~/.exports, ~/.aliases and ~/.functions
 # ~/.extra can be used for settings you don’t want to commit
-#for file in ~/.{extra,bash_prompt,exports,aliases,functions}; do
-#for file in ~/dotfiles/.{extra,bash_prompt,exports,aliases,functions}; do
-#
+# for file in ~/.{extra,bash_prompt,exports,aliases,functions}; do
+# for file in ~/dotfiles/.{extra,bash_prompt,exports,aliases,functions}; do
+##
 for file in ~/dotfiles/.{bash_prompt,aliases}; do
-  [ -r "$file" ] && source "$file"
+	[ -r "$file" ] && source "$file"
 done
 unset file
 
 # init z   https://github.com/rupa/z
 . ~/z/z.sh
+
+# init rvm
+source ~/.rvm/scripts/rvm
+
+# Case-insensitive globbing (used in pathname expansion)
+shopt -s nocaseglob
 
 # Prefer US English and use UTF-8
 export LC_ALL="en_US.UTF-8"
