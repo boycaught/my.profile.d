af2381ceb1a2e5338636b46d592ab1a5eb9bc2ef
LAG <github.robot@lagtime.com>
2016-01-17 14:11:18 -0800

    Update lag_custom.sh
    daily update, cleanup

--- lag_custom.sh
+++ lag_custom.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ## get version settings
-## updated to v_2016.01.04
+## updated to v_2016.01.17
 source ~/dotfiles/.appversions
 
 ## BASE PATHS
@@ -202,5 +202,5 @@ export LANG="en_US"
 # v_XXX files are application [v]ersion files, added to the ~/dotfiles/.appversions file
 
 #EOF
-#LastUpdated: 1/9/2016
-#v2016.01.09
+#LastUpdated: 1/17/2016
+#v2016.01.17
